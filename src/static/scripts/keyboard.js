f8ff75fb4840504fd283a6dbd169a9e49be1029c
Andrii Alieshchev <vghn78@motorolasolutions.com>
2019-04-13 17:56:13 +0200

    changed front reduced number of inputs changed buffering approach start/stop

--- src/static/scripts/keyboard.js
+++ src/static/scripts/keyboard.js
@@ -4,16 +4,37 @@ import {processMetrics} from './metrics.js'
 
 const metricsSocket = new IOSocket();
 
-const keyDowns = rxjs.fromEvent(document, 'keydown');
-const keyUps = rxjs.fromEvent(document, 'keyup');
+const getKeyActionsStream = element => {
+    const keyDowns$ = rxjs.fromEvent(element, 'keydown');
+    const keyUps$ = rxjs.fromEvent(element, 'keyup');
+    const keyActions$ = rxjs.merge(keyDowns$, keyUps$)
 
-const keyActions = rxjs.merge(keyDowns, keyUps)
+    return keyActions$;
+}
+
+const getClickStream = msg => selector => {
+    const element = document.querySelector(selector);
+
+    if (!element) {
+        throw Error(`${selector} element not found!`);
+    }
+
+    const start$ = rxjs
+        .fromEvent(element, 'click');
+    console.log(msg, element);
+
+    return start$;
+}
+
+const start = getClickStream('Starting keydown and keyup logging on');
+const stop = getClickStream('Stopping keydown and keyup logging on');
 
 const compareEvents = (a, b) => a.type === b.type && a.key === b.key
 
-const stream = keyActions
-    .pipe(rxjs.operators.distinctUntilChanged(compareEvents),
-        rxjs.operators.filter(e => ![' ', 'Shift', 'Tab', 'Backspace'].includes(e.key)),
+const getMetricsStream = (keyActionStream$, start$, stop$) => {
+
+    const metricsStream$ = keyActionStream$.pipe(rxjs.operators.distinctUntilChanged(compareEvents),
+        rxjs.operators.filter(e => !['Shift', 'Tab', 'Backspace'].includes(e.key)),
         rxjs.operators.map(e => {
             return {
                 type: e.type,
@@ -23,14 +44,31 @@ const stream = keyActions
                 login: getCookie('login')
             }
         }),
-        rxjs.operators.bufferCount(30));
+        rxjs.operators.bufferToggle(start$, () => stop$));
+    return metricsStream$;
+}
 
-stream.subscribe((metrics) => {
-    const processedMetrics = processMetrics(metrics)
-    console.dir(metrics);
-    console.dir(processedMetrics);
+export function attachKeylogger(inputSelector, startSelector, stopSelector) {
 
-    if (processedMetrics.length) {
-        metricsSocket.emitMetric(processedMetrics)
+    const element = document.querySelector(inputSelector);
+
+    if (!element) {
+        throw Error(`${inputSelector} element not found!`);
     }
-});
+
+    const $keyStream = getKeyActionsStream(element);
+    const start$ = start(startSelector);
+    const stop$ = stop(stopSelector);
+
+    const metricsStream$ = getMetricsStream($keyStream, start$, stop$);
+
+    metricsStream$.subscribe((metrics) => {
+        const processedMetrics = processMetrics(metrics)
+        console.dir(metrics);
+        console.dir(processedMetrics);
+
+        if (processedMetrics.length) {
+            metricsSocket.emitMetric(processedMetrics)
+        }
+    });
+}
